af5ba5ff73a54f68ffd044e060646d6381093103
Angel Vargas <angelvargas@outlook.es>
2020-11-02 22:15:03 +0000

    agregando base de configuracion para los Modelos

--- src/controllers/CrudController.ts
+++ src/controllers/CrudController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 // contract for CRUD operations
 export abstract class CrudController {
-    public abstract create(req: Resquest, res: Response): void;
+    public abstract create(req: Request, res: Response): void;
     public abstract read(req: Request, res: Response): void;
     public abstract update(req: Request, res: Response): void;
     public abstract delete(req: Request, res: Response): void;
